63f8d816d950f031aadf59889cd5e017f227ce5d
ViktorKorniyaka <korniyaka.viktor@gmail.com>
2013-04-09 16:00:29 +0300

    second

---
+++ DrawText.java
@@ -0,0 +1,14 @@
+
+
+
+public class DrawText implements Runnable{
+
+	
+	
+	public void run (){
+		
+		System.out.println("Thread # - " + Thread.currentThread());
+
+	}
+
+}
---
+++ MainClass.java
@@ -0,0 +1,24 @@
+
+public class MainClass {
+	
+	public void goDraw (){
+		DrawText draw = new DrawText ();
+		
+		Thread t1 = new Thread(draw);
+		Thread t2 = new Thread(draw);
+		t1.start();
+		t2.start();
+	}
+	
+	
+	public static void main (String[]args){
+		MainClass mc = new MainClass();
+		//for(int i = 0; i < 5; i++){
+			mc.goDraw();
+		//}
+		
+		//System.out.println("Thread t1 # - " + t1.getName());
+	
+	}
+
+}
